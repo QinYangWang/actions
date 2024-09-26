0c553abee281a4d9c38a1a245ce57becda921b09
QinYangWang <30904215+QinYangWang@users.noreply.github.com>
2024-09-26 13:58:02 +0800

    fix

--- image-sync/skopeo-sync.sh
+++ image-sync/skopeo-sync.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# Read the image list file
-IMAGES_FILE=$1
-
 # Username and password for the target registry
+DEST_REPOSITORY=$1
 DEST_USERNAME=$2
 DEST_PASSWORD=$3
 
+# Read the image list file
+IMAGES_FILE="$DEST_REPOSITORY.txt"
 # Check if username and password are empty
 if [ -z "$DEST_USERNAME" ]; then
     echo "Error: DEST_USERNAME is empty"
@@ -20,11 +20,12 @@ if [ -z "$DEST_PASSWORD" ]; then
     exit 1
 fi
 
+skopeo login $DEST_REPOSITORY --username=$DEST_USERNAME --password=$DEST_PASSWORD
 # Read the mirror list file line by line and synchronize the mirror
 while read IMAGE || [[ -n $IMAGE ]]; do
   SOURCE_IMAGE=$(echo $IMAGE | awk '{gsub(/^[ \t\r\n]+|[ \t\r\n]+$/, ""); print}' | awk '{print $1}')
   DEST_IMAGE=$(echo $IMAGE | awk '{gsub(/^[ \t\r\n]+|[ \t\r\n]+$/, ""); print}' | awk '{print $2}')
   echo "======================================================================================================================================="
   echo "Syncing $SOURCE_IMAGE to $DEST_IMAGE"
-  skopeo copy --dest-creds $DEST_USERNAME:$DEST_PASSWORD docker://$SOURCE_IMAGE docker://$DEST_IMAGE
+  skopeo copy docker://$SOURCE_IMAGE docker://$DEST_IMAGE
 done < "$IMAGES_FILE"
